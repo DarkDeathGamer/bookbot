f1f561052e7f016c50bcb8f202bcb9f4b0c34e4b
Nico Lepping <lenico1997@live.de>
2025-08-17 18:14:19 +0200

    add Print a Report

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from stats import get_word_count
+from stats import get_char_count
+from stats import sort_dict
 
 def get_book_text(book_dir: str) -> str:
     with open(book_dir) as f:
         return f.read()
 
+def print_stats(text):
+    wc = get_word_count(text)
+    cc = get_char_count(text)
+    sort_cc = sort_dict(cc)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {wc} total words")
+    print("--------- Character Count -------")
+    for char_count in sort_cc:
+        print(f"{char_count['char']}: {char_count['num']}")
+
+
 def main():
     book_dir = "./books/frankenstein.txt"
     text = get_book_text(book_dir)
-    wc = get_word_count(text)
-    print(f"{wc} words found in the document")
+    print_stats(text)
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,27 @@
-def get_word_count(text: str) -> int:
+def sort_on(items):
+    return items["num"]
+
+def get_word_count(text):
     return len(text.split())
+
+def get_char_count(text):
+    lower = text.lower()
+    count = {}
+
+    for char in lower:
+        if not (char in count):
+            count[char] = 1
+        else:
+            count[char] += 1
+    
+    return count
+
+def sort_dict(counts):
+    count_list = []
+    for count in counts:
+        temp_dict = {"char": "", "num": 0}
+        temp_dict["char"] = count
+        temp_dict["num"] = counts[count]
+        count_list.append(temp_dict)
+    count_list.sort(reverse=True, key=sort_on)
+    return count_list
